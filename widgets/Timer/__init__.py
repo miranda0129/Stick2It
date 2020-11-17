9d0f87f710fa082758c9b7ee6b950628b68a371d
bachittle <bachittle@gmail.com>
2020-11-17 03:50:29 -0500

    a timer that somewhat functions

--- widgets/Timer/__init__.py
+++ widgets/Timer/__init__.py
@@ -1,11 +1,48 @@
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, QObject, pyqtSignal, QThread
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from widgets import global_vars as gv
+from widgets.Timer.timer import Timer 
 
 qtcreator_file = "widgets/Timer/timer.ui"  # Enter file here.
 Ui_TimerWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
+import time
+
+class Timer(QObject):
+    finished = pyqtSignal()     # dont put this in __init__, it has to be outside
+
+    def __init__(self, parent=None, timerEdit=None):
+        if timerEdit:
+            self.timerEdit = timerEdit
+        QObject.__init__(self, parent=parent)
+        self.continue_run = True
+    
+    def countdown(self):
+        print("starting countdown...")
+        if not self.timerEdit:
+            print("no timerEdit... please add it.")
+        t = self.strToSec(self.timerEdit.text())
+        while t != 0 and self.continue_run:
+            mins, secs = divmod(t, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            self.timerEdit.setText(timeformat)
+            QThread.sleep(1)
+            t -= 1
+        self.continue_run = True
+        self.finished.emit()
+
+    def strToSec(self, s):
+        mins = s[:2]
+        secs = s[3:]
+        return int(mins) * 60 + int(secs)
+    
+    def stop(self):
+        print("stopping timer...")
+        self.continue_run = False
+
+
 class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
+    stop_signal = pyqtSignal()      # dont put this in __init__, it has to be outside
     def __init__(self):
         # name widget for window management
         self.name = "widget4"
@@ -15,8 +52,25 @@ class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
         Ui_TimerWindow.__init__(self)
         self.setupUi(self)
 
-        # yea, dynamic resizing would be better, but for now this will do. Lots of room, will fit exactly how I make the qt design
-        self.setFixedSize(615, 600)
+        # Thread:
+        self.thread = QThread()
+        self.timer = Timer(timerEdit = self.timerEdit)
+        self.stop_signal.connect(self.timer.stop)  # connect stop signal to worker stop method
+        self.timer.moveToThread(self.thread)
+
+        #self.timer.finished.connect(self.thread.quit)  # connect the workers finished signal to stop thread
+        #self.timer.finished.connect(self.timer.deleteLater)  # connect the workers finished signal to clean up worker
+        #self.thread.finished.connect(self.thread.deleteLater)  # connect threads finished signal to clean up thread
+
+        self.thread.started.connect(self.timer.countdown)
+        self.thread.finished.connect(self.timer.stop)
+
+        # Start Button action:
+        self.startButton.clicked.connect(self.thread.start)
+
+        # Stop Button action:
+        self.stopButton.clicked.connect(self.stop_signal.emit)
+    
 
     # close event will update global var tracking open widgets
     def closeEvent(self, event):
@@ -24,3 +78,6 @@ class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
         print(gv.open_widgets)
         del gv.open_widgets[self.name]
         print(gv.open_widgets)
+
+
+
