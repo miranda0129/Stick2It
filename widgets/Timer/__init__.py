f322e28bac877307ec5be38e0886dc5421797644
bachittle <bachittle@gmail.com>
2020-11-22 16:19:00 -0500

    refactored timer for optimization

--- widgets/Timer/__init__.py
+++ widgets/Timer/__init__.py
@@ -8,34 +8,32 @@ Ui_TimerWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
 import time
 
-class Timer(QObject):
-    finished = pyqtSignal()     # dont put this in __init__, it has to be outside
+class Timer(QThread):
+    stop_signal = pyqtSignal()     # dont put this in __init__, it has to be outside
 
-    def __init__(self, parent=None, window=None):
+    def __init__(self, window=None):
+        QThread.__init__(self)
+        self.stop_signal.connect(self.stop)
         if window:
             self.window = window 
-        QObject.__init__(self, parent=parent)
-        self.continue_run = True
     
-    def countdown(self):
+    def run(self):
         print("starting countdown...")
         if not self.window.timerEdit:
             print("no timerEdit... please add it.")
         t = self.strToSec(self.window.timerEdit.text())
+
+        self.continue_run = True
         while t != 0 and self.continue_run:
             mins, secs = divmod(t, 60)
             timeformat = '{:02d}:{:02d}'.format(mins, secs)
             self.window.timerEdit.setText(timeformat)
             QThread.sleep(1)
             t -= 1
-        print(t)
         if t == 0:
             print("countdown done!")
             self.window.timerEdit.setText("00:00")
             self.window.notify("done!")
-        self.continue_run = True
-        self.window.runningTimer = False
-        self.finished.emit()
 
     def strToSec(self, s):
         mins = s[:2]
@@ -53,43 +51,30 @@ class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
     def __init__(self):
         # name widget for window management
         self.name = "widget4"
-        self.runningTimer = False
 
         # load and set up ui file
         QtWidgets.QMainWindow.__init__(self)
         Ui_TimerWindow.__init__(self)
         self.setupUi(self)
         self.statusLabel.hide()
-        self.startButton.clicked.connect(self.timerInit)
+
+        # Start Button action:
+        self.startButton.clicked.connect(self.start_timer)
+
+        # Stop Button action:
+        self.stopButton.clicked.connect(self.stop_timer)
 
     # code is bad and I should feel bad, multiple thread spawns
     # fix this when time persists
-    def timerInit(self):
+    def start_timer(self):
         self.statusLabel.hide()
-        if not self.runningTimer:
-            self.runningTimer = True
-            # Thread:
-            self.thread = QThread()
-            self.timer = Timer(window = self)
-            self.stop_signal.connect(self.timer.stop)  # connect stop signal to worker stop method
-            self.timer.moveToThread(self.thread)
-
-            self.timer.finished.connect(self.thread.quit)  # connect the workers finished signal to stop thread
-            self.timer.finished.connect(self.timer.deleteLater)  # connect the workers finished signal to clean up worker
-            self.thread.finished.connect(self.thread.deleteLater)  # connect threads finished signal to clean up thread
-
-            self.thread.started.connect(self.timer.countdown)
-            self.thread.finished.connect(self.timer.stop)
-
-            # Start Button action:
-            self.thread.start()
-
-            # Stop Button action:
-            self.stopButton.clicked.connect(self.stop_timer)
-        
-    def stop_timer(self):
-        self.stop_signal.emit()
+        self.timer = Timer(window=self)
+        self.timer.finished.connect(self.timer.quit)  # connect the workers finished signal to stop thread
+        self.timer.finished.connect(self.timer.deleteLater)  # connect the workers finished signal to stop thread
+        self.timer.start()
     
+    def stop_timer(self):
+        self.timer.stop_signal.emit()
 
     # close event will update global var tracking open widgets
     def closeEvent(self, event):
