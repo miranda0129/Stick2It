ae3e03adbe8a2ed78f24a28298f2d9636206b65d
bachittle <bachittle@gmail.com>
2020-11-24 21:48:34 -0500

    custom timer times

--- widgets/Timer/__init__.py
+++ widgets/Timer/__init__.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QSize, QObject, pyqtSignal, QThread
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from widgets import global_vars as gv
+from widgets.Timer.objects import Timer
 import os.path
 
 qtcreator_file = "widgets/Timer/timer.ui"  # Enter file here.
@@ -9,7 +10,7 @@ Ui_TimerWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 import time
 
 """
-TimerThread: creates a specific QThread that counts down from a Timer object. 
+TimerThread: creates a specific QThread that counts down a time. 
 """
 class TimerThread(QThread):
     stop_signal = pyqtSignal()     # dont put this in __init__, it has to be outside
@@ -56,17 +57,18 @@ managing different timers stored as a list of timer objects
 """
 class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
     def __init__(self):
-        # name widget for window management
-        self.name = "widget4"
-
         # load and set up ui file
         QtWidgets.QMainWindow.__init__(self)
         Ui_TimerWindow.__init__(self)
         self.setupUi(self)
         self.statusLabel.hide()
 
+        # name widget for window management
+        self.name = "widget4"
         self.load_timers()
 
+        # TODO: start/stop toggler instead of separate buttons
+        self.timerEdit.returnPressed.connect(self.start_timer)  # enter key triggers start
         self.startButton.clicked.connect(self.start_timer)
         self.stopButton.clicked.connect(self.stop_timer)
         self.addTimer.clicked.connect(self.add_timer)
@@ -75,6 +77,9 @@ class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
         load_timers: if a time file exists, then load it up at startup. If not, then be a blank startup. 
     """
     def load_timers(self):
+        self.timers = []    # an array of Timer objects, with an associated button
+        self.index = -1      # which Timer of the array is loaded into the widget
+
         self.btnGroup = QtWidgets.QButtonGroup()
         self.btnGroup.setExclusive(True)
         default_file = 'widgets/Timer/data/timer1.json'
@@ -85,13 +90,57 @@ class Window(QtWidgets.QMainWindow, Ui_TimerWindow):
             self.add_timer(is_checked=True)
     
     def add_timer(self, is_checked=False):
-        btn = QtWidgets.QPushButton("Default Timer")
+        timer = Timer(name=f"timer {len(self.timers)}")
+        self.timers.append(timer)
+        btn = QtWidgets.QPushButton(timer.name)
+        btn.clicked.connect(self.init_timer)
         btn.setCheckable(True)
+        self.timerHBox.addWidget(btn)
+        self.btnGroup.addButton(btn, len(self.timers) - 1)
         if is_checked:
             btn.setChecked(True)
-        self.timerHBox.addWidget(btn)
-        self.btnGroup.addButton(btn)
+            self.init_timer()
         
+    """
+        init_timer: after a button toggle is toggled on, run this function.
+            it uses the QButton's associated ID in the QButtonGroup, and
+            opens the associated button in the self.timers list. 
+
+            it also replaces current timer data in the old index at self.index.  
+    """
+    def init_timer(self):
+        print("--------")
+        new_index = self.btnGroup.checkedId()
+        if new_index != -1:
+            if self.index == new_index: 
+                raise Exception("new timer index same as old timer index. An error has occured. ")
+            
+            # replace self.index with current timer time (and name)
+            old_timer = self.timers[self.index]
+            new_timer = self.timers[new_index]
+            print(old_timer)
+            print(new_timer)
+            if self.index != -1:
+                old_timer.times = [self.timerEdit.text()]
+
+            print("--------")
+            print(old_timer)
+            print(new_timer)
+            # move to new_index
+            self.timerEdit.setText(new_timer.times[0])
+            for i in range(len(new_timer.times)):
+                print(f"time at index {i}: {new_timer.times[i]}")
+            
+            self.index = new_index
+
+    """
+        boolean to toggle starting and stopping
+            True: change text of button to stop, in countdown state
+            False: change text of button to start, in edit state
+    """
+    def start_stop_toggler(self):
+        pass
+
     def start_timer(self):
         self.statusLabel.hide()
         self.timer = TimerThread(window=self)
