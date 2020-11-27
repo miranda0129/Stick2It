796a8a0dc380009e85985389302e59af7f0af7c7
joseb37 <guille11@uwindsor.ca>
2020-11-26 20:59:22 -0500

    Updated Budgeter with more functionality, changed the ui file, and started a budgeter test file

--- widgets/Budgeter/__init__.py
+++ widgets/Budgeter/__init__.py
@@ -7,6 +7,7 @@ from widgets import global_vars as gv
 qtcreator_file = "widgets/Budgeter/budgeter.ui"  # Enter file here.
 Ui_BudgetWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
+
 class Window(QtWidgets.QMainWindow, Ui_BudgetWindow):
     def __init__(self):
         # name widget for window management
@@ -17,13 +18,16 @@ class Window(QtWidgets.QMainWindow, Ui_BudgetWindow):
         Ui_BudgetWindow.__init__(self)
         self.setupUi(self)
 
-        #connect budgetButton to function when clicked
+        # connect budgetButton to function when clicked
         self.budgetButton.clicked.connect(self.calculateBudget)
+
+        # connect summaryButton to function when clicked
+        self.summaryButton.clicked.connect(self.calculateSummary)
 
     def calculateBudget(self):
         temp_costs = self.fixedCost.text().replace(' ', '')
         temp_income = self.expectedIncome.text().replace(' ', '')
-        temp_savings = self.savings.text().replace(' ','')
+        temp_savings = self.savings.text().replace(' ', '')
 
         if len(temp_costs) > 0:
             costs = temp_costs.split(",")
@@ -45,7 +49,7 @@ class Window(QtWidgets.QMainWindow, Ui_BudgetWindow):
 
         if savings[-1] == '%':  # case where saving is a percent of income
             percentage = float(savings.replace('%', '')) / 100
-            saved = percentage*total_income
+            saved = percentage * total_income
         else:
             saved = float(savings)
 
@@ -55,6 +59,40 @@ class Window(QtWidgets.QMainWindow, Ui_BudgetWindow):
         self.result.setText(spending_str)
 
     # close event will update global var tracking open widgets
+
+    def calculateSummary(self):
+
+        temp_costs = self.fixedCost.text().replace(' ', '')
+        temp_income = self.expectedIncome.text().replace(' ', '')
+        temp_savings = self.savings.text().replace(' ', '')
+
+        if len(temp_costs) > 0:
+            costs = temp_costs.split(",")
+            # returns sum of the fixed costs, accepts list of numbers or name: value pairs
+            total_cost = sum([float(cost.split(":")[-1]) for cost in costs])
+        else:
+            total_cost = 0
+
+        if len(temp_income) > 0:
+            incomes = temp_income.split(",")
+            total_income = sum([float(income.split(":")[-1]) for income in incomes])
+        else:
+            total_income = 0
+
+        if len(temp_savings) > 0:
+            savings = temp_savings
+        else:
+            savings = "0"
+
+        if savings[-1] == '%':  # case where saving is a percent of income
+            percentage = float(savings.replace('%', '')) / 100
+            saved = percentage * total_income
+        else:
+            saved = float(savings)
+
+        summary_str = "Total Income: ${:.2f}, Total Cost: ${:.2f}".format(total_income, total_cost)
+        self.summary.setText(summary_str)
+
     def closeEvent(self, event):
         print("closing window...")
         print(gv.open_widgets)
