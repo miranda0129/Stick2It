44240a5ce657bb486bcd73b21528df7fb41e6984
bachittle <39804642+bachittle@users.noreply.github.com>
2020-11-29 15:27:16 -0500

    fixed tests

--- test/TimerTest.py
+++ test/TimerTest.py
@@ -19,24 +19,28 @@ class TimerTest(unittest.TestCase):
     def test_case1(self):
         print("test setup")
         QTest.mouseClick(window.widget4, Qt.LeftButton)
-        print(f"current window: {gv.current_window}")
-        QTest.mouseClick(gv.current_window, Qt.LeftButton)
+        print(f"current window: {gv.window}")
+        QTest.mouseClick(gv.window, Qt.LeftButton)
 
     # create 4 or 5 sample timers
     def test_case2(self):
+        tl = gv.window.timesLayout
+        for i in range(tl.count()):
+            t = tl.itemAt(i).widget()
+            t.setText("00:01")
         print("test timers")
-        gv.current_window.timerEdit.setText("00:01")
+
 
     # start and stop them at different intervals
     def test_case3(self):
         print("test start stop")
-        QTest.mouseClick(gv.current_window.startButton, Qt.LeftButton)
-        spy = QSignalSpy(gv.current_window.notify)
+        QTest.mouseClick(gv.window.toggleButton, Qt.LeftButton)
+        spy = QSignalSpy(gv.window.notify)
         print(f"Spy signal: {spy.wait(timeout=10000)}")
 
     # look for done popup
     def test_case4(self):
-        self.assertTrue(gv.current_window.statusLabel.isVisible())
+        self.assertTrue(gv.window.statusLabel.isVisible())
 
     # create a new file
     # save it and exit
